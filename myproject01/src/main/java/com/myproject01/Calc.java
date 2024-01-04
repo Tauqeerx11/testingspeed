e625ce74c6ecfd1f412d434d2fce6589a75d6a1b
MohammedTauqeerShare <MohammedTauqeerShare@Rev-PF3C6CJB>
2024-01-04 10:40:29 -0500

    MyCalc

---
+++ myproject01/src/main/java/com/myproject01/Calc.java
@@ -0,0 +1,16 @@
+package com.myproject01;
+
+public class Calc {
+	public int addition(int i,int j) {
+		return i+j;
+	}
+	public int subtraction(int i,int j) {
+		return i-j;
+	}
+	public int multiplication(int i,int j) {
+		return i*j;
+	}
+	public int division(int i,int j) {
+		return i/j;
+	}
+}
---
+++ myproject01/src/test/java/com/myproject01/CalcTest.java
@@ -0,0 +1,54 @@
+package com.myproject01;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalcTest {
+	
+	
+	Calc c;
+	//setup method is responsible to initialize the resources
+	@Before
+	public void setUp() throws Exception {
+		c=new Calc(); 
+	}
+	//tear down method is responsible to release the resources
+	@After
+	public void tearDown() throws Exception {
+		c=null;
+	}
+
+	//Its a test case
+	@Test
+	public void testAddition() {
+		int result=c.addition(5, 7);
+		
+		assert result>6: "value should be greater than 6"; 
+	
+	}
+
+	@Test
+	public void testSubtraction() {
+		int result=c.subtraction(5, 7);
+		
+		assert result<6: "value should be greater than 6"; 
+	}
+
+	@Test
+	public void testMultiplication() {
+		int result=c.multiplication(5, 7);
+		
+		assert result>6: "value should be greater than 6"; 
+	}
+
+	@Test
+	public void testDivision() {
+		int result=c.division(5, 7);
+		
+		assert result<6: "value should be greater than 6"; 
+	}
+
+}
